994daece92d01843b378d6710df441919794ea2f
Hamed Zargaripour <zargaripour@gmail.com>
2019-07-13 01:45:21 +0430

    reformat

--- jquery.linkedSelect.js
+++ jquery.linkedSelect.js
@@ -1,103 +1,105 @@
 /*!
- * Item: linkedSelect 1.1.0 (https://github.com/zargaripour/linkedSelect)
+ * Item: linkedSelect 1.2.0 (https://github.com/zargaripour/linkedSelect.js)
  * Author: Hamed Zargaripour (https://zargaripour.com)
  * Licensed under MIT
  */
-(function(n) {
-    n.fn.linkedSelect = function(data, toggle, options) {
+(function (n) {
+    n.fn.linkedSelect = function (data, toggle, options) {
 
         options = (typeof options === 'undefined') ? {} : options;
 
         var opt = [];
-        opt['force']        = (options['force'])        ? options['force']      :false;
-        opt['text']         = (options['text'])         ? options['text']       :"Choose your option";
-        opt['framework']    = (options['framework'])    ? options['framework']  :null;
+        opt['force'] = (options['force']) ? options['force'] : false;
+        opt['text'] = (options['text']) ? options['text'] : "Choose your option";
+        opt['framework'] = (options['framework']) ? options['framework'] : null;
 
-        if(!data || !toggle){
+        if (!data || !toggle) {
             return null;
-        }
-        else {
-            var $group = $('[data-toggle="'+ toggle +'"]');
+        } else {
+            var $group = $('[data-toggle="' + toggle + '"]');
 
-            function select(list,level) {
-                var $thisElement    = $group.filter('[data-level="'+ level +'"]');
-                var defaultKey      = $thisElement.attr('data-value');
-                var defaultExist    = false;
+            function select(list, level) {
+                var $thisElement = $group.filter('[data-level="' + level + '"]');
+                var defaultKey = $thisElement.attr('data-value');
+                var defaultExist = false;
 
-                if($thisElement.length > 0) {
+                if ($thisElement.length > 0) {
 
                     var tempLevel = level;
                     do {
-                        if ($group.filter('[data-level="'+ tempLevel +'"]').length > 0)
-                            {
-                                $group.filter('[data-level="'+ tempLevel +'"]').find('option').remove();
-                                if (opt['force'] == true)
-                                    $group.filter('[data-level="'+ tempLevel +'"]').append($('<option value="" disabled selected>' + opt['text'] + '</option>'));
-                                else
-                                    $group.filter('[data-level="'+ tempLevel +'"]').append($('<option value="">' + opt['text'] + '</option>'));
-                            }
-                        else
+                        if ($group.filter('[data-level="' + tempLevel + '"]').length > 0) {
+                            $group.filter('[data-level="' + tempLevel + '"]').find('option').remove();
+                            if (opt['force'] === true)
+                                $group
+                                    .filter('[data-level="' + tempLevel + '"]')
+                                    .append($('<option value="" disabled selected>' + opt['text'] + '</option>'));
+                            else
+                                $group
+                                    .filter('[data-level="' + tempLevel + '"]')
+                                    .append($('<option value="">' + opt['text'] + '</option>'));
+                        } else
                             break;
                         tempLevel++;
-                    }while(true);
+                    } while (true);
 
                     if (typeof list === 'object') {
 
                         $.each(list, function (key, value) {
                             var fixedValue = null;
 
-                            if(value['__title'] == undefined)
+                            if (value['__title'] === undefined)
                                 fixedValue = value;
                             else
                                 fixedValue = value['__title'];
 
-                            if(key == '__title') {
+                            if (key === '__title') {
                                 //skip the title
-                            }
-                            else if(key == defaultKey) {
+                            } else if (key === defaultKey) {
                                 $thisElement.append($('<option></option>')
                                     .attr('value', key)
                                     .attr('selected', 'selected')
                                     .text(fixedValue));
                                 defaultExist = true;
-                            }
-                            else {
+                            } else {
                                 $thisElement.append($('<option></option>')
                                     .attr('value', key)
                                     .text(fixedValue));
                             }
                         });
 
-                        if (defaultExist == true){
+                        if (defaultExist === true) {
                             list = ListGenerator(data, level);
                             select(list, level + 1);
                         }
                     }
                 }
 
-                if (opt['framework'] == "materialize")
+                if (opt['framework'] === "materialize")
                     $('select').material_select('update');
             }
 
+            /* create new object list for each level from main object list */
             function ListGenerator(data, level) {
                 var list = data;
-
-                for (var index = 0; index <= level ; index++ ) {
+                for (var index = 0; index <= level; index++) {
                     var SelectedValue = $group.filter('[data-level="' + index + '"]').val();
                     list = list[SelectedValue];
                 }
-
                 return list;
             }
 
-            $group.filter('[data-level]').change(function() {
-                var level   = parseInt($(this).attr('data-level'));
-                var list    = ListGenerator(data, level);
+            /* run function when user change the option */
+            $group.filter('[data-level]').change(function () {
+                /* find out witch level has been changed */
+                var level = parseInt($(this).attr('data-level'));
+                /* prepare the choices */
+                var list = ListGenerator(data, level);
+                /* prepare next choices */
                 select(list, level + 1);
             });
 
+            /* start to fill the first level choices */
             select(data, 0);
         }
     };
 })(jQuery);
-
